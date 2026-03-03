05ba39fbc96fbb054dbeaacebb1494c3d563cfa7
jshuay <jshuay@users.noreply.github.com>
2026-03-03 08:00:22 -0800

    Implement Account and Deposit

--- src/payment_system/account.rs
+++ src/payment_system/account.rs
@@ -7,3 +7,42 @@ pub struct Account {
     held: Amount,
     locked: bool,
 }
+
+impl Account {
+    pub fn new(client_id: ClientId) -> Self {
+        Account {
+            client_id,
+            available: Amount::ZERO,
+            held: Amount::ZERO,
+            locked: false,
+        }
+    }
+
+    pub fn client_id(&self) -> &ClientId {
+        &self.client_id
+    }
+
+    pub fn available_funds(&self) -> &Amount {
+        &self.available
+    }
+
+    pub fn available_funds_mut(&mut self) -> &mut Amount {
+        &mut self.available
+    }
+
+    pub fn held_funds(&self) -> &Amount {
+        &self.held
+    }
+
+    pub fn held_funds_mut(&mut self) -> &mut Amount {
+        &mut self.held
+    }
+
+    pub fn total_funds(&self) -> Amount {
+        self.available + self.held
+    }
+
+    pub fn locked(&self) -> bool {
+        self.locked
+    }
+}
--- src/payment_system/deposit.rs
+++ src/payment_system/deposit.rs
@@ -7,3 +7,25 @@ pub struct Deposit {
     transaction_id: TransactionId,
     amount: Amount,
 }
+
+impl Deposit {
+    pub fn new(client_id: ClientId, transaction_id: TransactionId, amount: Amount) -> Self {
+        Deposit {
+            client_id,
+            transaction_id,
+            amount,
+        }
+    }
+
+    pub fn client_id(&self) -> &ClientId {
+        &self.client_id
+    }
+
+    pub fn transaction_id(&self) -> &TransactionId {
+        &self.transaction_id
+    }
+
+    pub fn amount(&self) -> &Amount {
+        &self.amount
+    }
+}
