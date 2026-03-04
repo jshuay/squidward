951d32b012d0e38b9acefdc8e0ae0b28a710a2d5
jshuay <jshuay@users.noreply.github.com>
2026-03-04 00:25:30 -0800

    Add unit tests for valid/invalid csv file paths

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod payment_system;
 
+use std::fs::File;
 use std::path::PathBuf;
 use std::process::ExitCode;
 
 use clap::Parser;
+use csv::Reader;
 use csv::Trim;
 use log::LevelFilter;
 use log::debug;
@@ -19,6 +21,9 @@ struct Args {
     debug: bool,
 }
 
+/// Program entry point.
+///
+/// This main function's primary job is to setup the CLI component as well as validating the input.
 fn main() -> ExitCode {
     let args = Args::parse();
 
@@ -33,8 +38,7 @@ fn main() -> ExitCode {
 
     debug!("Reading input transactions CSV file: {:?}", args.transactions_file);
 
-    let transactions_csv = csv::ReaderBuilder::new().trim(Trim::All).from_path(&args.transactions_file);
-
+    let transactions_csv = load_transactions_csv(&args.transactions_file);
     if let Err(error) = transactions_csv {
         error!("Failed to read transactions file: {}", error);
         return ExitCode::FAILURE;
@@ -43,4 +47,29 @@ fn main() -> ExitCode {
     payment_system::simulate(transactions_csv.unwrap());
 
     ExitCode::SUCCESS
+}
+
+fn load_transactions_csv(transactions_file: &PathBuf) -> csv::Result<Reader<File>> {
+    csv::ReaderBuilder::new().trim(Trim::All).from_path(transactions_file)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use crate::load_transactions_csv;
+
+    #[test]
+    fn load_transactions_csv_with_valid_file_path() {
+        let valid_csv_path = PathBuf::from("test_files/valid.csv");
+
+        assert!(load_transactions_csv(&valid_csv_path).is_ok());
+    }
+
+    #[test]
+    fn load_transactions_csv_with_invalid_file_path_returns_error() {
+        let valid_csv_path = PathBuf::from("test_files/invalid_file_path.csv");
+
+        assert!(load_transactions_csv(&valid_csv_path).is_err());
+    }
 }
